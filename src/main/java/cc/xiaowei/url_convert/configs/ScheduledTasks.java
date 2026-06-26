ea7919126258d9c34de21c224a893f2318bcb3d3
xiaowei-ce <starryflow@outlook.com>
2026-06-27 03:24:07 +0800

    commit message

--- src/main/java/cc/xiaowei/url_convert/configs/ScheduledTasks.java
+++ src/main/java/cc/xiaowei/url_convert/configs/ScheduledTasks.java
@@ -6,12 +6,9 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.time.YearMonth;
 
 
